8d434d06c8ad023548c21f59d5d7b838c0b5583d
Shuki Levy <shukile@cisco.com>
2021-03-25 20:06:57 +0200

    Trigger build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi there!dy</h1>
+      <h1>Hi thejre!dy</h1>
       <hr />
       <MarketingApp />
     </div>
